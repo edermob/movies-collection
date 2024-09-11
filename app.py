b2908f716dc203bff679ef6188a1fceb2dfd25b9
eder <ederqueiroz4444@gmail.com>
2024-09-11 12:28:29 -0300

    front end ok

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ API_URL = "http://127.0.0.1:5000/api/movies"
 def index():
     if request.method == 'POST':
         title = request.form['title']
-        genre = request.foem['genre']
+        genre = request.form['genre']
         requests.post(API_URL, json={'title':title, 'genre':genre}
         )
         return redirect('/')
@@ -20,6 +20,27 @@ def index():
         else:
             movies = []
     return render_template('index.html', movies=movies)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    requests.delete(f'{API_URL}/{id}')
+    return  redirect('/')
+
+@app.route('/update/<int:id>')
+def update(id):
+    if request.method == 'POST':
+        title = request.get['title']
+        genre = request.get['genre']
+        requests.put(f'{API_URL}/{id}', json={'title':title, 'genre': genre})
+        return redirect('/')
+    
+    response = requests.get(f'{API_URL}/{id}')
+    if response.status_code == 200:
+        movie = response.json()
+    else:
+        movie = {}
+
+    return render_template('update.html', movie=movie)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
